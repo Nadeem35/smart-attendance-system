eb08fcfa7acb994f258299036008675cbd71032d
nadeem35 <nadeem.cse72@gmail.com>
2025-12-14 13:01:02 +0530

    fix blank page issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./Component/Home/Home";
 import AdminLogin from "./Component/Admin/AdminLogin";
 import StudentLogin from "./Component/Student/StudentLogin";
@@ -9,15 +9,15 @@ import TeacherLogin from "./Component/Teacher/TeacherLogin";
 const App = () => {
   return (
     <>
-      <BrowserRouter>
-        {/* <Home /> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/admin-login" element={<AdminLogin />} />
-          <Route path="/teacher-login" element={<TeacherLogin />} />
-          <Route path="/student-login" element={<StudentLogin />} />
-        </Routes>
-      </BrowserRouter>
+      {/* <BrowserRouter> */}
+      {/* <Home /> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/admin-login" element={<AdminLogin />} />
+        <Route path="/teacher-login" element={<TeacherLogin />} />
+        <Route path="/student-login" element={<StudentLogin />} />
+      </Routes>
+      {/* </BrowserRouter> */}
     </>
   );
 };
