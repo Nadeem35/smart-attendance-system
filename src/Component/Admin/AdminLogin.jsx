3e1105e0b8d134368ac2dc301b912ed66722a375
nadeem35 <nadeem.cse72@gmail.com>
2025-12-15 12:18:48 +0530

    Admin login page

--- src/Component/Admin/AdminLogin.jsx
+++ src/Component/Admin/AdminLogin.jsx
@@ -1,9 +1,57 @@
-import React from "react";
+import LogoTitle from "../LogoTitle";
 
 const AdminLogin = () => {
   return (
     <>
-      <h1 className="font-extrabold text-center">This Admin page</h1>
+      <div className="min-h-screen flex items-center justify-center">
+        {/* Background wrapper */}
+        <div
+          className="bg-none sm:bg-[url('https://agevole.in/wp-content/uploads/2023/07/wd-scaled.jpg')]
+          bg-cover bg-center bg-no-repeat w-full min-h-screen flx items-center justify-center"
+        >
+          <div className="w-full sm:w-1/2 min-h-screen sm:min-h-fit px-6 sm:px-10 py-12 bg-white/80 fle flex-col items-center sm:items-start">
+            <div className="px-15">
+              <LogoTitle />
+              <p className="font-semibold mt-3 text-(--text-gray) tracking-widest">
+                Lorem ipsum dolor sit amet.
+              </p>
+              <h2 className="font-bold text-2xl mt-8 tracking-widest">
+                Log In as <span className="text-(--heading-1)">Admin</span>
+              </h2>
+              <form className="mt-5 w-full">
+                {/* Email */}
+                <label className="text-(--text-gray) font-semibold tracking-widest">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  placeholder="e.g: aarfeen@gmail.com"
+                  className="bg-(--bg-light) block w-full rounded px-3 py-2 text-sm mt-2 mb-5 outline-none"
+                />
+
+                {/* Password */}
+                <label className="text-(--text-gray) font-semibold tracking-widest">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  placeholder="**********"
+                  className="bg-(--bg-light) block w-full rounded px-3 py-2 text-sm mt-2 mb-5 outline-none"
+                />
+
+                {/* Button */}
+                <button
+                  type="submit"
+                  className="bg-(--bg-dark) hover:bg-(--bg-dark)/90 w-full text-white rounded py-2 font-sembold cursor-pointer tracking-widest"
+                >
+                  Login
+                </button>
+              </form>
+            </div>
+            <div className="w-[50%] sm:w-fit mx-auto text-center"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -4,35 +4,20 @@ import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
+    <div className="font-sans min-h-screen flex items-center justify-center">
       {/* Background wrapper */}
       <div
-        className="
-          bg-none 
-          sm:bg-[url('https://agevole.in/wp-content/uploads/2023/07/wd-scaled.jpg')]
-          bg-cover bg-center bg-no-repeat
-          w-full min-h-screen
-          flx items-center justify-center
-        "
+        className="bg-none sm:bg-[url('https://agevole.in/wp-content/uploads/2023/07/wd-scaled.jpg')]
+          bg-cover bg-center bg-no-repeat w-full min-h-screen flx items-center justify-center"
       >
         {/* Content box */}
-        <div
-          className="
-            w-full sm:w-1/2
-            min-h-screen sm:min-h-fit
-            px-6 sm:px-10 py-12
-            bg-white/80
-            fle flex-col
-            items-center sm:items-start
-          "
-        >
-          <LogoTitle />
-
-          <div className="my-10">
-            <h1 className="font-sans font-bold text-3xl sm:text-4xl">
-              Login As
-            </h1>
-            <p className="mt-3 text-gray-600">
+        <div className="w-full sm:w-1/2 min-h-screen sm:min-h-fit px-6 sm:px-10 py-12 bg-white/80 fle flex-col items-center sm:items-start">
+          <div className="w-fit mx-auto text-center">
+            <LogoTitle />
+          </div>
+          <div className="my-10 text-center">
+            <h1 className="font-bold text-3xl sm:text-4xl">Login As</h1>
+            <p className="mt-3 text-(--text-gray)">
               Lorem ipsum dolor, sit amet consectetur adipisicing elit.
               Perferendis unde architecto minima.
             </p>
@@ -41,19 +26,19 @@ const Home = () => {
           <div className="w-[50%] sm:w-fit mx-auto text-center">
             <Link
               to="/admin-login"
-              className="text-center bg-gray-300 hover:bg-blue-600 hover:text-white block px-8 sm:px-15 py-1 rounded"
+              className="text-center bg-(--bg-light) hover:bg-(--bg-dark) hover:text-white block px-8 sm:px-15 py-1 rounded"
             >
               Admin
             </Link>
             <Link
               to="/teacher-login"
-              className="text-center bg-gray-300 hover:bg-blue-600 hover:text-white block px-8 sm:px-15 py-1 rounded my-4"
+              className="text-center bg-(--bg-light) hover:bg-(--bg-dark) hover:text-white block px-8 sm:px-15 py-1 rounded my-4"
             >
               Teacher
             </Link>
             <Link
               to="/student-login"
-              className="text-center bg-gray-300 hover:bg-blue-600 hover:text-white block px-8 sm:px-15 py-1 rounded"
+              className="text-center bg-(--bg-light) hover:bg-(--bg-dark) hover:text-white block px-8 sm:px-15 py-1 rounded"
             >
               Student
             </Link>
