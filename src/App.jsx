f7a03ceadaaccb25a458aff4ca716a61f2c4abe8
nadeem35 <nadeem.cse72@gmail.com>
2025-12-17 22:34:58 +0530

    remove extra pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Component/Home/Home";
-import AdminLogin from "./Component/Admin/AdminLogin";
-import StudentLogin from "./Component/Student/StudentLogin";
-import TeacherLogin from "./Component/Teacher/TeacherLogin";
+// import Home from "./Component/Home/Home";
 import SignUp from "./Component/Account/SignUp";
 import ForgotPassword from "./Component/Account/ForgotPassword";
 import LoginAccount from "./Component/Account/LoginAccount";
@@ -19,13 +16,11 @@ const App = () => {
         <Route path="/sign-up" element={<SignUp />} />{/*---- SignUp page ----*/}
         <Route path="/login-account" element={<LoginAccount />} /> {/*---- Home page ----*/}
         <Route path="/reset-password-page" element={<ResetPasswordPage />} /> {/*---- Home page ----*/}
-
-        <Route path="/Home" element={<Home />} />
-        {/* <Route path="/" element={<AdminLogin />} /> ---- Home page ---- */}
-        <Route path="/teacher-login" element={<TeacherLogin />} />
-        <Route path="/student-login" element={<StudentLogin />} />
-        {/* <Route path="/admin-sign-up" element={<AdminSignUp />} /> */}
         <Route path="/forgot-password" element={<ForgotPassword />} />
+
+        {/* <Route path="/Home" element={<Home />} /> */}
+        {/* <Route path="/teacher-login" element={<TeacherLogin />} /> */}
+        {/* <Route path="/student-login" element={<StudentLogin />} /> */}
       </Routes>
       {/* </BrowserRouter> */}
     </>
