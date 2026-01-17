5cad9296a3aa9fb601219a801dc779c5e8ff9c94
nadeem35 <nadeem.cse72@gmail.com>
2026-01-17 22:55:50 +0530

    change design

--- src/Component/EmployeeProfile.jsx
+++ src/Component/EmployeeProfile.jsx
@@ -43,21 +43,23 @@ const EmployeeProfile = () => {
           return (
             <div
               key={photo.id}   // ✅ ID FROM API
-              className="w-70 bg-blue-600/60 p-4 text-white rounded-xl border-4 border-blue-600/30 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+              className="w-70 bg-blue-600/60 text-white rounded-xl border-4 border-blue-600/30 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
             >
               {/* ✅ API IMAGE */}
               <img
                 src={user.products[0]?.thumbnail}
                 alt={user.id}
-                className="w-24 h-24 rounded-full mx-auto mb-4 bg-white/50 border-5 border-t-amber-500 border-b-amber-500 border-r-green-500 border-l-green-500 object-cover"
+                className="w-fit h-fit rounded-fll mx-auto mb-4 bg-white/80 borde-5 border-t-amber-500 border-b-amber-500 border-r-green-500 border-l-green-500 object-cover"
               />
-              {/* ✅ MIXED DATA CORRECTLY */}
-              <p><strong>API ID:</strong> {user.id}</p>
-              <p><strong>Title:</strong> {user.products[0]?.title}</p>
-              <p><strong>Price:</strong> {user.products[0]?.price}</p>
-              <p><strong>Quantity:</strong> {user.products[0]?.quantity}</p>
-              <p><strong>Total Price:</strong> {user.products[0]?.total}</p>
-              <p><strong>Discount Percentage:</strong> {user.products[0]?.discountPercentage}</p>
+              <div className="p-4">
+                {/* ✅ MIXED DATA CORRECTLY */}
+                <p><strong>API ID:</strong> {user.id}</p>
+                <p><strong>Title:</strong> {user.products[0]?.title}</p>
+                <p><strong>Price:</strong> {user.products[0]?.price}</p>
+                <p><strong>Quantity:</strong> {user.products[0]?.quantity}</p>
+                <p><strong>Total Price:</strong> {user.products[0]?.total}</p>
+                <p><strong>Discount Percentage:</strong> {user.products[0]?.discountPercentage}</p>
+              </div>
             </div>
           );
         })}
